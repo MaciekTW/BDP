c1a2504d26565dffb6d7ea64ba39607e03875fdc
Maciek <twarogmaciej2000@gmail.com>
2022-11-22 23:58:38 +0100

    Lab5

--- Lab5/lab4.sql
+++ Lab5/lab4.sql
@@ -1,2 +1,4 @@
-insert into obiekty values (7, ST_COLLECT(array[(select geometry from obiekty where name='obiekt3'),
-												(select geometry from obiekty where name='obiekt4')]), 'obiekt7');
+insert into obiekty 
+values 
+(7, ST_COLLECT(array[(select geometry from obiekty where name='obiekt3'),
+(select geometry from obiekty where name='obiekt4')]), 'obiekt7');
